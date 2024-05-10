22963c8e386319dfc629488add50af3b22adf853
quorafind <951011105@qq.com>
2024-05-10 11:52:30 +0800

    feat: support return back

--- main.ts
+++ main.ts
@@ -1,13 +1,72 @@
-import { Plugin } from 'obsidian';
+import { Plugin, setIcon } from 'obsidian';
+
+declare module 'Obsidian' {
+	interface WorkspaceRibbon {
+		ribbonSettingEl: HTMLElement;
+
+		makeRibbonItemButton(icon: string, tooltip: string, onClick: (e: MouseEvent) => void): HTMLElement;
+	}
+
+	interface App {
+		setting: {
+			open: () => void;
+		};
+
+		openVaultChooser(): void;
+
+		openHelp(): void;
+	}
+}
 
 export default class MyPlugin extends Plugin {
+	ribbonMap: Map<string, HTMLElement> = new Map<string, HTMLElement>();
 
 	async onload() {
-
+		this.initVaultSwitcher();
 	}
 
 	onunload() {
+		this.ribbonMap.forEach((value, key) => {
+			value.detach();
+		});
+		this.ribbonMap.clear();
+	}
 
+	initVaultSwitcher() {
+		const leftRibbon = this.app.workspace.leftRibbon;
+		const ribbonVaultSwitchEl = leftRibbon.makeRibbonItemButton("vault", 'Switch vault', (e) => {
+				e.stopPropagation();
+				this.app.openVaultChooser();
+			}
+		);
+
+		ribbonVaultSwitchEl.addEventListener("mouseover", () => {
+				setIcon(ribbonVaultSwitchEl, "open-vault");
+			}
+		);
+		ribbonVaultSwitchEl.addEventListener("mouseout", () => {
+				setIcon(ribbonVaultSwitchEl, "vault");
+			}
+		);
+		leftRibbon.ribbonSettingEl.appendChild(ribbonVaultSwitchEl);
+
+		const ribbonHelpEl = leftRibbon.makeRibbonItemButton("help", 'Help', (e) => {
+				e.stopPropagation();
+				app.openHelp();
+			}
+		);
+		leftRibbon.ribbonSettingEl.appendChild(ribbonHelpEl);
+
+		const ribbonSettingEl = leftRibbon.makeRibbonItemButton("lucide-settings", 'Settings', (e) => {
+				e.stopPropagation();
+				app.setting.open();
+			}
+		);
+		leftRibbon.ribbonSettingEl.appendChild(ribbonSettingEl);
+
+		this.ribbonMap.set("vault", ribbonVaultSwitchEl);
+		this.ribbonMap.set("help", ribbonHelpEl);
+		this.ribbonMap.set("lucide-settings", ribbonSettingEl);
 	}
 
 }
